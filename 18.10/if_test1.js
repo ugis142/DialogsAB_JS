2c0ed4e116503e68d3a6c0b6e4b59e18f91589e5
ugis142 <ugis142@inbox.lv>
2021-10-18 20:29:28 +0300

    update

--- 18.10/if_test1.js
+++ 18.10/if_test1.js
@@ -1,1 +1,20 @@
-if (new Date().getHours() <18
+/*
+if (new Date().getHours() <18) {
+    document.getElementById("demo").innerHTML = "Good day!";
+}
+*/
+function compare() {
+    var a = document.getElementById("aval").value;
+    var b = document.getElementById("bval").value;
+    var conclusion = "";
+
+    if (a > b) {
+        conclusion = "a (" + a + ") is bigger than b (" + b + ")";
+    }
+    else if (a < b) {
+        conclusion = "a (" + a + ") is smaller than b (" + b + ")";
+    }
+    else {
+        conclusion = "a (" + a + ") is equal to b (" + b + ")";
+    }
+    document.getElementById("demo").innerHTML = conclusion}
