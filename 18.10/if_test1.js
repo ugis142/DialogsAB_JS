bcb39ca89f45cf80c82a5fed303ec88bbeef7042
ugis142 <ugis142@inbox.lv>
2021-10-18 21:05:19 +0300

    123

--- 18.10/if_test1.js
+++ 18.10/if_test1.js
@@ -3,7 +3,8 @@ if (new Date().getHours() <18) {
     document.getElementById("demo").innerHTML = "Good day!";
 }
 */
-function compare() {
+
+function compare_two() {
     var a = document.getElementById("aval").value;
     var b = document.getElementById("bval").value;
     var conclusion = "";
@@ -18,3 +19,17 @@ function compare() {
         conclusion = "a (" + a + ") is equal to b (" + b + ")";
     }
     document.getElementById("demo").innerHTML = conclusion}
+
+    function compare_three() {
+        var a = document.getElementById("aval").value;
+        var b = document.getElementById("bval").value;
+        var c = document.getElementById("cval").value;
+        var conclusion = "...";
+        var temp;
+        /*...*/
+    
+        if (a > b){temp = a; a = b; b = temp;}
+        if (b > c){temp = b; b = c; c = temp;}
+        if (a > b){temp = a; a = b; b = temp;}
+
+        document.getElementById("demo").innerHTML = a + " " + " " + b + " " + c; }
